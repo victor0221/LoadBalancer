0a11917378e19f1c8ce568856afec0bb974a8f96
victor_dev <victorplu09@gmail.com>
2024-04-17 23:34:28 +0100

    model and config

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,37 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 public class Main {
 
 public static void main(String[] args) {
-        final int PORT = 42069; //<---- DONT TOUCH WILL BREAK EVERYTHING
+        Scanner hostCap = new Scanner(System.in);
+        System.out.println("Enter your host number (e.g. 'localhost'): ");
+        String activeHost = hostCap.nextLine();
+        Scanner portCap = new Scanner(System.in);
+        System.out.println("Enter port number: ");
+        int activePort = portCap.nextInt();
+        Config config = new Config(activePort,activeHost);
         
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
-            System.out.println("Load balancer started. Listening on port " + PORT);
+            System.out.println("Load balancer started. Listening on port " + config.getPort());
+            ServerSocket serverSocket = new ServerSocket(config.getPort());
+            
+            //block below will capture sent data from job sender.
+            //FYI BufferedReader is a package specifct to Strings to make it easier for the 
+            // program to read it.
+            Socket clientSocket = serverSocket.accept();
+            BufferedReader r = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String jb = r.readLine();
+            System.out.println("jobSender said...: " + jb);
+            clientSocket.close();
             
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                //still needs doing... probs best to handle client connection in new thread.
-                new Thread(new LoadBalancerHandler(clientSocket)).start();
+                //foreshadowing...
+                //Socket clientSocket = serverSocket.accept();
+               // new Thread(new LoadBalancerHandler(clientSocket)).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
