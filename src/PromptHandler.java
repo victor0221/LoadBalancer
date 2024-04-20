b15bfd14bf647beebc463eb079a053e3ed8eb48b
victor_dev <victorplu09@gmail.com>
2024-04-20 14:02:45 +0100

    load balancer

---
+++ src/PromptHandler.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author usula
+ */
+public class PromptHandler {
+    public PromptHandler(){
+        
+    }
+    
+    public void handlePrompt(String code, int optionalInt, String optionalString){
+        switch(code){
+            case "lbRunning":
+                System.out.println("Load balancer running ("+optionalInt+")");
+                break;
+            case "noMoreJobs":
+                System.out.println("No more jobs from JobSender.");
+                break;
+            case "roundRobin":
+                System.out.println("Distributing Job (" + optionalInt + "ms) to " + optionalString+ " node.");
+                break;
+            case "noNodes":
+                System.out.println("No nodes to handle job.");
+                break;
+            case "failedJob":
+                System.out.println("Failed to send job to "+optionalString);
+                break;
+            case "projectHeader":
+                System.out.println("---PROJECT CONFIGURATION SECTION---");
+                break;
+            case "host":
+                 System.out.println("Enter your host number (e.g. 'localhost'): ");
+                break;
+            case "port":
+                System.out.println("Enter port number: ");
+                break;
+            case "nodeHeader":
+                System.out.println("---NODE CONFIGURATION---");
+                break;
+            case "nodeAmount":
+                System.out.println("Enter amount of nodes you want to connect to: ");
+                break;
+            case "nodeName":
+                System.out.println("Enter node name: ");
+                break;
+            case "nodeHost":
+                System.out.println("Enter node host (e.g. 'localhost'):");
+                break;
+            case "nodePort":
+                System.out.println("Enter node port number:");
+                break;
+            case "nodeSuccess":
+                System.out.println("node added!");
+                break;
+            case "pressY":
+                System.out.println("enter 'y' to view stack:");
+                break;
+        }
+    }
+}
---
+++ src/WorkerNodeTemplate.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usula
+ */
+public class WorkerNodeTemplate implements IWorkerNodeTemplate {
+    private String _nodeName;
+    private String _nodeHost;
+    private int _nodePort;
+
+    //default constructor
+    public WorkerNodeTemplate() {
+    }
+    
+    public WorkerNodeTemplate(String nodeName, String nodeHost, int nodePort) {
+        _nodeName = nodeName;
+        _nodeHost = nodeHost;
+        _nodePort = nodePort;
+    }
+
+    public String getNodeName() {
+        return _nodeName;
+    }
+
+    public String getNodeHost() {
+        return _nodeHost;
+    }
+
+    public int getNodePort() {
+        return _nodePort;
+    }
+
+    public void setNodeName(String nodeName) {
+        _nodeName = nodeName;
+    }
+
+    public void setNodeHost(String nodeHost) {
+        _nodeHost = nodeHost;
+    }
+
+    public void setNodePort(int nodePort) {
+        _nodePort = nodePort;
+    }
+}
